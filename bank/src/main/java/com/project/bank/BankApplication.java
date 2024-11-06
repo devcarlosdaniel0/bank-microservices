6b907e193ed05122e382c49f2401d07db4cf96b0
Carlos <cadanielmarchesin@gmail.com>
2024-11-06 19:54:03 -0300

    criação do Discovery e Gateway

---
+++ bank/src/main/java/com/project/bank/BankApplication.java
@@ -0,0 +1,16 @@
+package com.project.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplication.class, args);
+	}
+
+}
