cccb381849ee03b9d4148c6ed20f07e4f3133902
Carlos <cadanielmarchesin@gmail.com>
2024-12-16 16:46:36 -0300

    criando o email service

---
+++ mail/src/main/java/com/project/mail/service/EmailSenderService.java
@@ -0,0 +1,30 @@
+package com.project.mail.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class EmailSenderService {
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String emailFrom;
+
+    public void sendEmail(String toEmail, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(emailFrom);
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+
+        log.info("Sending email from {} to {}", emailFrom, toEmail);
+    }
+}
