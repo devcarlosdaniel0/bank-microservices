b636b3032d77170c6a3a32525411edb3bc1d5795
Carlos <cadanielmarchesin@gmail.com>
2024-12-18 15:55:09 -0300

    fazendo o envio de email ser assincrono

---
+++ mail/src/main/java/com/project/mail/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.project.mail.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
--- mail/src/main/java/com/project/mail/service/EmailSenderService.java
+++ mail/src/main/java/com/project/mail/service/EmailSenderService.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +17,7 @@ public class EmailSenderService {
     @Value("${spring.mail.username}")
     private String emailFrom;
 
+    @Async
     public void sendEmail(String toEmail, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(emailFrom);
