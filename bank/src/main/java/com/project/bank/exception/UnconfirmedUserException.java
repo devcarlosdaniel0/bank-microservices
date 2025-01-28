be386096158df52bd26fc7f8a79b1e700b5d3fd1
Carlos Daniel <cadanielmarchesin@gmail.com>
2025-01-28 13:31:58 -0300

    Documentando o método create bank account

--- bank/src/main/java/com/project/bank/exception/UnconfirmedUserException.java
+++ bank/src/main/java/com/project/bank/exception/UnconfirmedUserException.java
@@ -3,7 +3,7 @@ package com.project.bank.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class UnconfirmedUserException extends RuntimeException {
     public UnconfirmedUserException(String message) {
         super(message);
