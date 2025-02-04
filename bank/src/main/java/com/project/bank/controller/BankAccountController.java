4390227be78b6a2ee5067218002232e86396c1d5
Carlos Daniel <cadanielmarchesin@gmail.com>
2025-02-04 15:22:36 -0300

    documentando withdrawal

--- bank/src/main/java/com/project/bank/controller/BankAccountController.java
+++ bank/src/main/java/com/project/bank/controller/BankAccountController.java
@@ -22,7 +22,7 @@ public class BankAccountController {
     @PostMapping("create-bank-account")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Bank account created"),
-            @ApiResponse(responseCode = "404", description = "User id from JWT token not found"),
+            @ApiResponse(responseCode = "404", description = "User ID from JWT token not found"),
             @ApiResponse(responseCode = "409", description = "Bank account already exists"),
             @ApiResponse(responseCode = "422", description = "Invalid currency code"),
             @ApiResponse(responseCode = "403", description = "User not confirmed"),
@@ -39,6 +39,10 @@ public class BankAccountController {
     }
 
     @PostMapping("withdrawal-balance")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Withdrawal success"),
+            @ApiResponse(responseCode = "400", description = "Insufficient funds to withdrawal")
+    })
     public ResponseEntity<BankAccountResponseDTO> withdrawalBalance(@RequestBody @Valid UpdateBalanceDTO updateBalanceDTO) {
         return new ResponseEntity<>(bankAccountService.withdrawalBalance(updateBalanceDTO), HttpStatus.OK);
     }
@@ -53,7 +57,7 @@ public class BankAccountController {
             @ApiResponse(responseCode = "200", description = "Transfer success"),
             @ApiResponse(responseCode = "403", description = "Transfer not allowed (ex: attempt to transfer to your own bank account or transfer value <= 0)"),
             @ApiResponse(responseCode = "400", description = "Insufficient funds to complete the transfer"),
-            @ApiResponse(responseCode = "404", description = "User ID or receiver account email not found")
+            @ApiResponse(responseCode = "404", description = "User ID from token or receiver account email not found")
     })
     public ResponseEntity<TransferResponseDTO> transfer(@RequestBody @Valid TransferDTO transferDTO) {
         return new ResponseEntity<>(bankAccountService.transfer(transferDTO), HttpStatus.OK);
