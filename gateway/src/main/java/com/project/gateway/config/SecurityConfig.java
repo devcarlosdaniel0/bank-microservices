ea1474f18737d1b824b705c81eeb0a3f4070c91c
Carlos Daniel <cadanielmarchesin@gmail.com>
2026-01-08 10:08:42 -0300

    adding swager on gateway and routing currency api

--- gateway/src/main/java/com/project/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/project/gateway/config/SecurityConfig.java
@@ -14,7 +14,14 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http.csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/eureka/**").permitAll()
+                        .pathMatchers(
+                                "/eureka/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/*/v3/api-docs/**",
+                                "/webjars/**")
+                        .permitAll()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
