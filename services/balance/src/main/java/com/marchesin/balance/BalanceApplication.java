f2ff765c79a7b765156eb5901e2115a8e8323840
Carlos Daniel <cadanielmarchesin@gmail.com>
2026-01-21 17:53:32 -0300

    kafka setup

--- services/balance/src/main/java/com/marchesin/balance/BalanceApplication.java
+++ services/balance/src/main/java/com/marchesin/balance/BalanceApplication.java
@@ -2,8 +2,10 @@ package com.marchesin.balance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BalanceApplication {
 
 	public static void main(String[] args) {
