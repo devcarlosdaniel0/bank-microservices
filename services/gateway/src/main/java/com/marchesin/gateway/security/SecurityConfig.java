6fb13e123f538e0db82c4e304ffce2435f60993c
Carlos Daniel <cadanielmarchesin@gmail.com>
2026-03-17 13:09:34 -0300

    adding swagger docs in gateway

--- services/gateway/src/main/java/com/marchesin/gateway/security/SecurityConfig.java
+++ services/gateway/src/main/java/com/marchesin/gateway/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.marchesin.gateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -12,9 +13,18 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/eureka/**").permitAll()
+                        .pathMatchers(HttpMethod.OPTIONS).permitAll()
+                        .pathMatchers(
+                                "/eureka/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/*/v3/api-docs/**",
+                                "/webjars/**")
+                        .permitAll()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
